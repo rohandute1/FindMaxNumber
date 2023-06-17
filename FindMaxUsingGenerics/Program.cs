393258c27aa1cc3d1f262c6586a661ec7eec8c47
Rohan <rohandute1@gmail.com>
2023-06-17 17:40:14 +0530

    [Rohan] Successfully checked which one is max number

--- FindMaxUsingGenerics/Program.cs
+++ FindMaxUsingGenerics/Program.cs
@@ -11,7 +11,35 @@ namespace FindMaxUsingGenerics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Find maximum number using generics");
+            int num1, num2, num3;
+
+            Console.WriteLine("Enter the first number:");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number:");
+            num3 = int.Parse(Console.ReadLine());
+
+            int max = GetMax(num1, num2, num3);
+
+            Console.WriteLine("Maximum number: " + max);
+
             Console.ReadLine();
+        }
+
+        static int GetMax(int num1, int num2, int num3)
+        {
+            int max = num1;
+
+            if (num2 > max)
+                max = num2;
+
+            if (num3 > max)
+                max = num3;
+
+            return max;
         }
     }
 }
