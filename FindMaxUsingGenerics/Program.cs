5bf271b07e7124a26c27e5256d4b5466d6a81385
Rohan <rohandute1@gmail.com>
2023-06-17 17:30:30 +0530

    [Rohan] Successfully added welcome message

---
+++ FindMaxUsingGenerics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxUsingGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find maximum number using generics");
+            Console.ReadLine();
+        }
+    }
+}
