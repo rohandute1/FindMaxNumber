5fba500fe07438e09a6bf69680dfcb389f0052a9
Rohan <rohandute1@gmail.com>
2023-06-18 13:28:34 +0530

    [Rohan] Successfully added PrintMax Method

--- FindMaxUsingGenerics/Program.cs
+++ FindMaxUsingGenerics/Program.cs
@@ -15,34 +15,47 @@ namespace FindMaxUsingGenerics
 
             Console.WriteLine("Test case 1:");
             int result1 = GetMax(30, 10, 25);
-            Console.WriteLine("Max Number: " + result1);
+            PrintMax(result1);
+            Console.WriteLine("Given max number at 1st position: " + result1);
 
             Console.WriteLine("Test case 2:");
             int result2 = GetMax(20, 33, 50, 15);
-            Console.WriteLine("Max Number: " + result2);
+            PrintMax(result2);
+            Console.WriteLine("Given max number at 2nd position: " + result2);
 
             Console.WriteLine("Test case 3:");
             float result3 = GetMax(30.5f, 10.2f, 25.8f);
-            Console.WriteLine("Max Float: " + result3);
+            PrintMax(result3);
+            Console.WriteLine("Given max number at 3rd position: " + result3);
 
             Console.WriteLine("Test case 4:");
             float result4 = GetMax(20.1f, 33.7f, 50.9f, 15.3f);
-            Console.WriteLine("Max Float: " + result4);
+            PrintMax(result4);
+            Console.WriteLine("Given max number at 4th position: " + result4);
 
             Console.WriteLine("Test case 5:");
             string result5 = GetMax("Apple", "Peach", "Banana");
-            Console.WriteLine("Max string: " + result5);
+            PrintMax(result5);
+            Console.WriteLine("Given max string at 5th position: " + result5);
 
             Console.WriteLine("Test case 6:");
             string result6 = GetMax("Orange", "Grapes", "Watermelon", "Kiwi");
-            Console.WriteLine("Max string: " + result6);
+            PrintMax(result6);
+            Console.WriteLine("Given max string at 6th position: " + result6);
 
             Console.ReadLine();
         }
 
         static T GetMax<T>(params T[] values) where T : IComparable<T>
         {
-            return values.Max();
+            T max = values.Max();
+            PrintMax(max);
+            return max;
+        }
+
+        static void PrintMax<T>(T max)
+        {
+            Console.WriteLine("Maximum value: " + max);
         }
     }
 }
