de1a06f80b9d2736052c177b42e53b4f3e2c67bf
Rohan <rohandute1@gmail.com>
2023-06-18 13:17:56 +0530

    [Rohan] Successfully added multiple input to check max number

--- FindMaxUsingGenerics/Program.cs
+++ FindMaxUsingGenerics/Program.cs
@@ -14,68 +14,35 @@ namespace FindMaxUsingGenerics
             Console.WriteLine("Welcome to Find maximum using generics");
 
             Console.WriteLine("Test case 1:");
-            int result1 = GetMax<int>(30, 10, 25);
-            Console.WriteLine("Given max number at 1st position: " + result1);
+            int result1 = GetMax(30, 10, 25);
+            Console.WriteLine("Max Number: " + result1);
 
             Console.WriteLine("Test case 2:");
-            float result2 = GetMax<float>(20.1f, 33.7f, 50.9f);
-            Console.WriteLine("Given max number at 2nd position: " + result2);
+            int result2 = GetMax(20, 33, 50, 15);
+            Console.WriteLine("Max Number: " + result2);
 
             Console.WriteLine("Test case 3:");
-            string result3 = GetMax<string>("Apple", "Peach", "Banana");
-            Console.WriteLine("Given max string at 3rd position: " + result3);
+            float result3 = GetMax(30.5f, 10.2f, 25.8f);
+            Console.WriteLine("Max Float: " + result3);
 
             Console.WriteLine("Test case 4:");
-            GenericClass<int> genericInt = new GenericClass<int>(30, 10, 25);
-            int result4 = genericInt.TestMaximum();
-            Console.WriteLine("Given max number using Generic Class: " + result4);
+            float result4 = GetMax(20.1f, 33.7f, 50.9f, 15.3f);
+            Console.WriteLine("Max Float: " + result4);
+
+            Console.WriteLine("Test case 5:");
+            string result5 = GetMax("Apple", "Peach", "Banana");
+            Console.WriteLine("Max string: " + result5);
+
+            Console.WriteLine("Test case 6:");
+            string result6 = GetMax("Orange", "Grapes", "Watermelon", "Kiwi");
+            Console.WriteLine("Max string: " + result6);
 
             Console.ReadLine();
         }
 
-        static T GetMax<T>(T val1, T val2, T val3) where T : IComparable<T>
+        static T GetMax<T>(params T[] values) where T : IComparable<T>
         {
-            T max = val1;
-
-            if (val2.CompareTo(max) > 0)
-                max = val2;
-
-            if (val3.CompareTo(max) > 0)
-                max = val3;
-
-            return max;
-        }
-    }
-
-    class GenericClass<T> where T : IComparable<T>
-    {
-        private T val1;
-        private T val2;
-        private T val3;
-
-        public GenericClass(T val1, T val2, T val3)
-        {
-            this.val1 = val1;
-            this.val2 = val2;
-            this.val3 = val3;
-        }
-
-        public T TestMaximum()
-        {
-            return GetMax(val1, val2, val3);
-        }
-
-        private T GetMax(T val1, T val2, T val3)
-        {
-            T max = val1;
-
-            if (val2.CompareTo(max) > 0)
-                max = val2;
-
-            if (val3.CompareTo(max) > 0)
-                max = val3;
-
-            return max;
+            return values.Max();
         }
     }
 }
